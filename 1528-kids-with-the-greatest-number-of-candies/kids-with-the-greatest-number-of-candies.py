ee2f621eb631f5d023b476968c55f411b92cd1d1
GURUKIRAN C S <157163749+Gurukiran603@users.noreply.github.com>
2026-03-23 23:13:36 +0530

    Refactor kidsWithCandies to use a loop for results
    Refactor kidsWithCandies method for clarity and performance.

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -2,3 +2,19 @@ class Solution:
     def kidsWithCandies(self, candies, extraCandies):
         max_candies = max(candies)
         return [c + extraCandies >= max_candies for c in candies]
+
+
+
+
+class Solution:
+    def kidsWithCandies(self, candies, extraCandies):
+        result = []
+        max_candies = max(candies)
+
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= max_candies:
+                result.append(True)
+            else:
+                result.append(False)
+
+        return result
