2c3ba3c95028c2a28d0cc1539876b0067958e4a5
GURUKIRAN C S <157163749+Gurukiran603@users.noreply.github.com>
2026-03-28 22:21:59 +0530

    Time: 16 ms (20.09%) | Memory: 13.5 MB (86.02%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count += (1 if num == candidate else -1)
+            return candidate
+            """
+        count = {}
+
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+            if count[num] > len(nums) // 2:
+                return num
